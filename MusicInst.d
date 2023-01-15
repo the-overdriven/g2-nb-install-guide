815f24ea969946b6666a1a557ec5dad0664194b4
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2023-01-15 19:46:12 +0100

    Update MusicInst.d
    restore original New World Monastery Indoor theme

--- MusicInst.d
+++ MusicInst.d
@@ -6,6 +6,23 @@ META
 
 instance NWP_DAY_STD(C_MUSICTHEME_STANDARD)
 {
-	file = "MAY_DayStd.sgt";
+	// New World Pyramids
+	// Returning: "Keeper_library.sgt"
+	file = "MI_DayStd.sgt";
+	// "MAY_DayStd.sgt";
 };
 
+instance MOD_NGT_STD(C_MUSICTHEME_STANDARD)
+{
+	// New World Monastery Indoor
+	// Returning: "ban_daystd.sgt";
+	file = "MI_DayStd.sgt";
+	
+};
+
+instance MOD_DAY_STD(C_MUSICTHEME_STANDARD)
+{
+	// New World Monastery Indoor
+	// Returning: "ban_daystd.sgt";
+	file = "MI_DayStd.sgt";
+};
